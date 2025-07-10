ebe60deb52c84851f3c6491d1617ff7564b3b2e8
hyelin <nachoego4@gmail.com>
2025-07-10 14:42:48 +0900

    파이썬 기초 실습

---
+++ 0710.py
@@ -0,0 +1,11 @@
+#printf("결과는 a") > a가 변수 의미 > 오류 > 변수명 f "결과는 {a}" 
+
+# product="coffee"
+# count=3
+# price=10000
+# print(f"상품 {product} {count}개의 가격은 {count*price}원입니다.")
+
+s="Monty Python"
+print(s[0]) #인덱싱(Indexing): 색인
+print(s[6:10]) #슬라이싱(slicing): 범위 추출
+#뒷숫자 -1 > 6~9
