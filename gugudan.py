87bbbd66b595144873aa5e5a0f1f721cd898bbc7
hyelin <nachoego4@gmail.com>
2025-07-17 14:46:15 +0900

    파이썬 반복문 while 연습

--- gugudan.py
+++ gugudan.py
@@ -9,12 +9,39 @@
 #     sum=sum+i
 # print(f"합계는 {sum}")
 
-a=int(input("원하는 수는: "))
-sum=0
-cnt=0
-for i in range(1,a+1,1):
-    if i%2==0:
-        sum=sum+i
-        cnt=cnt+1
-print(f"개수는 {cnt}")
-print(f"합계는 {sum}")
+# a=int(input("원하는 수는: "))
+# sum=0
+# cnt=0
+# for i in range(1,a+1,1):
+#     if i%2==0:
+#         sum=sum+i
+#         cnt=cnt+1
+# print(f"개수는 {cnt}")
+# print(f"합계는 {sum}")
+
+
+# for i in range(1,6):
+#     for j in range(0, i):
+#         print("*", end="")
+#     print()
+
+
+# for i in range(1,6):
+#     for j in range(6, i, -1):
+#         print("*", end="")
+#     print()
+
+# fruit=['참외', '파인애플', '사과', '바나나', '골드키위', '배']
+# cart=[]
+# for k in fruit:
+#     if len(k) >=3:
+#         cart.append(k)
+# print()
+
+과자={
+    "꼬깔콘":2000,
+    "새우깡":3830,
+    "포카칩":1180
+}
+for k, v in 과자.items():
+    print(k,v)
---
+++ while.py
@@ -0,0 +1,19 @@
+# password=""
+
+# while password !="pythonisfun":
+#     password=input("암호를 입력하시오: ")
+# print("로그인 성공")
+
+# sum=0
+# cnt=0
+# while cnt <=100:
+#     sum=sum+cnt
+#     cnt=cnt+1
+# print(f"합계는 {sum}")
+
+cnt=0
+while cnt<=4:
+    num=int(input("정수를 입력하시오: "))
+    if num%2==0:
+        cnt=cnt+1
+print("종료")
