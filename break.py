44d864aa61e7c6ccde532a48254769fbdaae6a99
hyelin <nachoego4@gmail.com>
2025-07-17 16:35:27 +0900

    반복문 break 연습

---
+++ break.py
@@ -0,0 +1,14 @@
+for i in range(0,10):
+    print(i)
+    if(i==4):
+        break
+print("4이면 탈출\n")
+
+while True:
+    a=input("종료를 입력하면 종료됩니다> ")
+    if a=="종료":
+        break
+print("종료되었습니다.")
+"""
+이것도 주석
+"""
